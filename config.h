f9c43aa8ab7fe7832e4a956ad7ede5a2c19eda6d
Ethan N <74840292+EthanN-200321@users.noreply.github.com>
2022-08-03 23:19:07 -0600

    Applied noborder patch

--- config.h
+++ config.h
@@ -5,18 +5,18 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 40;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 24;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *altbarcmd        = "$HOME/.dots/polybar/polybar.sh"; /* Alternate bar launch command */
+static const char *altbarcmd        = "$HOME/.dots/polybar.sh"; /* Alternate bar launch command */
 
 static const char *fonts[]          = { "Orienta-Regular:size=10" };
 static const char dmenufont[]       = "Orienta-Regular:size=10";
-static const char pink[]	    = "#D32CE6";
+static const char pink[]	    = "#e584f0";
 static const char black[]	    = "#101115";
 static const char black1[]		= "#0a0b0c"; // use 090a0b if too dark
 static const char white[]		= "#fafafa";
@@ -64,11 +64,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black1, "-nf", white, "-sb", black1, "-sf", pink, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/* Custom Functions */
+
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+//	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      spawn,           SHCMD("polybar-msg cmd toggle") },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -147,4 +151,6 @@ static IPCCommand ipccommands[] = {
   IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
   IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
 };
+
+
 
